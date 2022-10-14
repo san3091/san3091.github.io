e4905b13fb7034d6cd7cecad527eba9e05dc299d
Santiago <contact@santiagoqg.com>
2022-10-14 10:48:52 -0500

    switch to hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from 'react-router-dom';
 import Base from './Base'
@@ -11,7 +11,7 @@ import './index.css';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Base />,
